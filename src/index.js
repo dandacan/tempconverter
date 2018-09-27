ac94455d18ae64e64ca98767a12b0cb482b2a23a
Corey Johnson <31936720+corey-johnson@users.noreply.github.com>
2018-09-27 14:19:57 -0400

    F update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Fahrenheit from "./fahrenheit.js";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    value: 0
+  };
+
+  handleChange() {
+    this.setState({ input: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>Help!</div>
+        <input
+          vaule={this.state.value}
+          onChange={event => {
+            this.handleChange(event);
+          }}
+        />
+        <div>...Help!</div>
+        <Fahrenheit />
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
